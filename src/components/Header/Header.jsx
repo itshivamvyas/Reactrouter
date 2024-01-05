221defff3554b1e0b160e2419466502bf81ac404
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-01-05 18:55:31 +0530

    Make Header Better To Use

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+   const [showNavbar, setShowNavbar] = useState(true)
+
+   const onLinkClick = ()=>setShowNavbar(false)
+
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
+                    <Link to="/" className="flex items-center" onClick={()=>{setShowNavbar(!showNavbar)}}>
                         <p className="text-4xl font-bold text-gray-700">Card</p>
                         <span className="text-4xl font-bold text-orange-700">Nest</span>
                     </Link>
@@ -23,13 +28,17 @@ export default function Header() {
                             Get started
                         </Link>
                     </div>
+
+
+
                     <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        className={`${showNavbar ? "inherit": "hidden"} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                onClick={onLinkClick}
                                 to="/"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -41,6 +50,7 @@ export default function Header() {
 
                             <li>
                                 <NavLink
+                                onClick={onLinkClick}
                                 to="/about"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -52,6 +62,7 @@ export default function Header() {
 
                             <li>
                                 <NavLink
+                                onClick={onLinkClick}
                                 to="/contact-us"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
@@ -63,6 +74,7 @@ export default function Header() {
 
                             <li>
                                 <NavLink
+                                onClick={onLinkClick}
                                 to="/github"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
